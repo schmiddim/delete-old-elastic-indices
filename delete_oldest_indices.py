e565600534e48bdbf933b129459465886f64c53c
root <root@static.213.67.63.178.clients.your-server.de>
2018-02-08 10:01:28 +0100

    support for multiple indices

--- delete_oldest_indices.py
+++ delete_oldest_indices.py
@@ -6,13 +6,13 @@ import settings
 if __name__ == "__main__":
     es = Elasticsearch([settings.es_host])
     today = datetime.today()
-
-    for index in es.indices.get(settings.index_name + '*'):
-        str_index_date = index.replace(settings.index_name, "")
-        dateOfIndex = datetime.strptime(str_index_date, "%Y.%m.%d")
-        dateDiff = (today - dateOfIndex).days
-        if dateDiff > settings.keep_last_n_days:
-            print("DELETE ", index + " is ", (dateDiff), " days old")
-            es.indices.delete(index=index, ignore=[400, 404])
-        else:
-            print("KEEP   ", index + " is ", (dateDiff), " days old")
+    for index_to_delete in settings.index_names:
+        for index in es.indices.get(index_to_delete + '*'):
+            str_index_date = index.replace(index_to_delete, "")
+            dateOfIndex = datetime.strptime(str_index_date, "%Y.%m.%d")
+            dateDiff = (today - dateOfIndex).days
+            if dateDiff > settings.keep_last_n_days:
+                print("DELETE ", index + " is ", (dateDiff), " days old")
+                es.indices.delete(index=index, ignore=[400, 404])
+            else:
+                print("KEEP   ", index + " is ", (dateDiff), " days old")
